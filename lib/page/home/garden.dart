b503cad2096082ff8b5a28aa17da87c6d17ced06
lucasvaudey <lucasvaudey@gmail.com>
2022-06-11 15:20:46 +0200

    👌 IMPROVE: matrix animation now work

--- lib/page/home/garden.dart
+++ lib/page/home/garden.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-import 'package:vector_math/vector_math_64.dart' as vector;
-
 import 'package:flutter/material.dart';
 import 'package:mpp/widgets/garden/garden_piece.dart';
 
@@ -12,21 +9,23 @@ class Garden extends StatefulWidget {
 }
 
 class _GardenState extends State<Garden> with TickerProviderStateMixin {
-  late Animation<Matrix4> animationMatrix;
+  late Animation<double> animation10;
+  late Animation<double> animation01;
+  late Animation<double> animation11;
   AnimationController? controller;
+  Matrix4 endMatrix = Matrix4.identity()
+    ..setEntry(0, 0, 1)
+    ..setEntry(1, 0, -0.5)
+    ..setEntry(0, 1, 1)
+    ..setEntry(1, 1, 0.5);
 
   @override
   void initState() {
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 3));
-    animationMatrix = Matrix4Tween(
-      begin: Matrix4.identity(),
-      end: Matrix4.identity()
-        ..setEntry(0, 0, 1)
-        ..setEntry(1, 0, -0.5)
-        ..setEntry(0, 1, 1)
-        ..setEntry(1, 1, 0.5),
-    ).animate(controller!);
+    animation01 = Tween<double>(begin: 0, end: 1).animate(controller!);
+    animation11 = Tween<double>(begin: 1, end: 0.5).animate(controller!);
+    animation10 = Tween<double>(begin: 0, end: -0.5).animate(controller!);
 
     controller?.forward();
     super.initState();
@@ -40,44 +39,50 @@ class _GardenState extends State<Garden> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return InteractiveViewer(
-      maxScale: 10,
-      minScale: 0.4,
-      boundaryMargin: const EdgeInsets.all(double.infinity),
-      child: AnimatedBuilder(
-        animation: controller!,
-        builder: (context, anim) {
-          return Transform(
-            transform: animationMatrix.value,
-            child: Stack(
-              children: const [
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: GardenPiece(
-                    size: Size(100, 50),
-                    offset: Offset(0, 0),
-                  ),
-                ),
-                Positioned(
-                  top: 50,
-                  left: 100,
-                  child: GardenPiece(
-                    size: Size(50, 50),
-                    offset: Offset(0, 0),
-                  ),
-                ),
-                // Positioned(
-                //   top: 100 * (1 / 3) - (50 * (1 / 3) / 2),
-                //   left: 50,
-                //   child: GardenPiece(
-                //     size: Size(50, 50 * (1 / 3)),
-                //   ),
-                // ),
-              ],
-            ),
-          );
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller?.reset();
+          controller?.forward();
         },
+        child: Icon(Icons.redo),
+      ),
+      body: InteractiveViewer(
+        maxScale: 10,
+        minScale: 0.4,
+        boundaryMargin: const EdgeInsets.all(double.infinity),
+        child: AnimatedBuilder(
+          animation: controller!,
+          builder: (context, anim) {
+            return Transform(
+              transform: Matrix4.identity()
+                ..setEntry(0, 0, 1)
+                ..setEntry(1, 0, animation10.value)
+                ..setEntry(0, 1, animation01.value)
+                ..setEntry(1, 1, animation11.value),
+              child: Stack(
+                children: const [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: GardenPiece(
+                      size: Size(100, 50),
+                      offset: Offset(0, 0),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 100,
+                    child: GardenPiece(
+                      size: Size(50, 50),
+                      offset: Offset(0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
